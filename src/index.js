0e9d5b370837714b37bdaa064e8b8e6556bcf9fd
dima <d.ananyev@a1qa.com>
2020-10-05 14:43:12 +0400

    feat: implement solution for Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let stringValArr = n.toString().split('')
+  let resultArr = []
+  stringValArr.forEach(element => {
+    if (!(element == '-')) {
+      resultArr.push(element)
+    }
+  });
+  return Number(resultArr.slice().reverse().join(''))
 }
